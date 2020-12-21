a7796e580e77d133f1086e879e74597e3f0d792e
brunommbruno <brunommbruno@gmail.com>
2020-12-21 22:33:37 +0000

    created components folder and added App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+
+    return(
+      <h1>TITLE</h1>
+    )
+  }
+}
+export default App;
