f27b86162ba875bd92a2493e95f78eba0b445080
brunommbruno <brunommbruno@gmail.com>
2020-12-23 19:23:10 +0000

    added react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,9 @@
 import { Component } from "react";
+import{
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import StartScreen from "../StartScreen";
 import TeamCreator from "../TeamCreator";
@@ -9,18 +14,24 @@ class App extends Component {
   render(){
 
     return(
-      <div className={"container"}>
-        {!this.props.appIsOn ? 
-          <StartScreen /> 
-          : 
-          <TeamCreator />
-        }
-    
-        
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/">
+              <div className={"container"}>
+                {!this.props.appIsOn ? 
+                  <StartScreen /> 
+                  : 
+                  <TeamCreator />
+                }
+              </div>
+            </Route>
+          </Switch>
+        </>
+      </Router>
         
 
 
-      </div>
     )
   }
 }
