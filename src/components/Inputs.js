415dba32b9d681e4f5ec3ecbed565fd051539725
brunommbruno <brunommbruno@gmail.com>
2021-01-01 17:10:16 +0000

    added styling to playerInput and created inputs component

---
+++ src/components/Inputs.js
@@ -0,0 +1,60 @@
+import { Component } from "react";
+import {Form, ToggleButtonGroup, ToggleButton, Button, OverlayTrigger, Tooltip} from "react-bootstrap";
+
+class Inputs extends Component{
+
+
+    render(){
+
+        const renderTooltip = (props) => (
+            <Tooltip id="button-tooltip" {...props}>
+              Allows teams to be sorted by skill
+            </Tooltip>
+          )
+      
+
+        const { type, placeholder, handleInput, title, stateName, value, img, alt, handlePosition} = this.props
+
+        return(
+            <>
+                {type === "text" ? 
+                    <Form.Group controlId="formName">
+                        <Form.Label>{title}</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder={placeholder}
+                            onChange={(e) => handleInput(e, `${stateName}`)}
+                        />
+                    </Form.Group>
+                    :
+
+                type === "select" ? 
+                    <Form.Group>
+                        <Form.Label>{title}</Form.Label>                    
+                        <OverlayTrigger
+                            placement="right"
+                            delay={{show: 250, hide: 400}}
+                            overlay={renderTooltip}
+                        >
+                        <Button variant="info" className={"mb-2"} >?</Button>
+                        </OverlayTrigger>
+                        <Form.Control
+                            as="select"
+                            onChange={(e) => handleInput(e, `${stateName}`)}
+                        >
+                            <option>1</option>
+                            <option>2</option> 
+                            <option>3</option>  
+                        </Form.Control>
+                    </Form.Group>
+                    :
+
+                    null
+            }
+            </>
+        )
+    }
+
+}
+
+export default Inputs;
--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,27 +1,108 @@
 import { Component } from "react";
+import Inputs from "./Inputs";
+import {Container, Row, Col, Form, ToggleButtonGroup, ToggleButton, Button} from "react-bootstrap";
+
+import att from "../img/att.png";
+import mid from "../img/mid.png";
+import def from "../img/def.png";
+import gk from "../img/gk.png";
 
 class PlayerInput extends Component {
 
+
   render(){
 
-    const {handleInput} = this.props;
+    const {handleInput, handlePosition} = this.props;
 
     return(
         <>
-            <form>
-                <label>Name</label>
-                <input onChange={(e) => handleInput(e, "player_name")}></input>
+          <Container className={"text-center mt-5"}>
+              <Form>
+                <Row>
+                  <Col>
+                    <Inputs 
+                      type="text"
+                      placeholder="Steve"
+                      handleInput={handleInput}
+                      title="Name"
+                      stateName="player_name"
+                    />
+                  </Col>
 
-                <label>skill</label>
-                <input onChange={(e) => handleInput(e, "player_skill")}></input>
+                  <Col>
+                    <Inputs 
+                      type="select"
+                      handleInput={handleInput}
+                      title="Skill"
+                      stateName="player_skill"
+                    />
+                  </Col>
+                </Row>
+              </Form>
 
-                <label>position</label>
-                <input onChange={(e) => handleInput(e, "player_position")}></input>
-            </form>
+                <Row>
+                  
+                  <ToggleButtonGroup type="radio" name="options" defaultValue={1} className={"mt-4"}>
+                    <ToggleButton
+                      value={1}
+                      variant="transparent"
+                      onClick={(e) => handlePosition("gk", "player_position")}
+                    >
+                      <img 
+                          src={gk} 
+                          style={{height: "4rem",  borderRadius: "1rem"}}
+                          alt={"Goalkeeper Icon"}
+                        />
+                        <p className={"text-white"}>Goalkeeper</p>
+                  </ToggleButton>
 
-            <button onClick={this.props.handleAdd}>add</button>
+                  <ToggleButton
+                    value={2}
+                    variant="transparent"
+                    onClick={(e) => handlePosition("def", "player_position")}
+                  >
+                    <img 
+                        src={def} 
+                        style={{height: "4rem",  borderRadius: "1rem"}}
+                        alt={"Defender Icon"}
+                      />
+                      <p className={"text-white"}>Defender</p>
+                  </ToggleButton>
 
+                  <ToggleButton
+                    value={3}
+                    variant="transparent"
+                    onClick={(e) => handlePosition("mid", "player_position")}
+                  >
+                    <img 
+                        src={mid} 
+                        style={{height: "4rem",  borderRadius: "1rem"}}
+                        alt={"Midfielder Icon"}
+                      />
+                      <p className={"text-white"}>Midfielder</p>
+                  </ToggleButton>
 
+                  <ToggleButton
+                    value={4}
+                    variant="transparent"
+                    onClick={(e) => handlePosition("att", "player_position")}
+                  >
+                    <img 
+                        src={att} 
+                        style={{height: "4rem",  borderRadius: "1rem"}}
+                        alt={"Attacker Icon"}
+                      />
+                      <p className={"text-white"}>Attacker</p>
+                  </ToggleButton>
+                </ToggleButtonGroup>
+
+              <Button 
+                onClick={this.props.handleAdd}
+                style={{fontSize: "2rem"}}
+                className={"m-3 mt-1 button"}
+              >Add Player!</Button>
+              </Row>
+            </Container>
         </>
     )
   }
